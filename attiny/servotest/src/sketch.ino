aeb6d99c54350f0a72c9b433bc443b27141643ad
Tanner Hobson <thobson125@gmail.com>
2014-10-20 08:07:31 -0400

    Still unable to get Servo8Bit working

--- attiny/servotest/src/sketch.ino
+++ attiny/servotest/src/sketch.ino
@@ -1,104 +1,32 @@
-/*
-#define SIMPLE_TEST
-#define TIMING_TO_LED
-*/
-#define SERVO_TO_LED
-#if ARDUINO == 106
-  #define USE_ATTINY
-#endif
+#include "lib/Servo8Bit/Servo8Bit.h"
+#include <inttypes.h>
+#include <avr/interrupt.h>
+#include <avr/io.h>
 
-#ifdef USE_ATTINY
-  #include "lib/Servo8Bit/Servo8Bit.h"
-  typedef Servo8Bit Servo_t;
-  const int servoPin = 0;
-  const int ledPin = 2;
-#else
-//#include <Servo.h>
-  typedef Servo Servo_t;
-  const int servoPin = 0;
-  const int ledPin = 13;
-#endif
+const int servoPin = PB4;
+const int ledPin = 2;
+int ledValue = LOW;
+Servo8Bit servo;
+int counter = 0;
+int initialized = false;
 
-Servo_t servo;
-int pos;
-
-#ifdef SERVO_TO_LED
-  Servo_t servo2;
-#elif defined TIMING_TO_LED
-  int ledValue = HIGH;
-  const int ledPulseDuration = 100; /* milliseconds */
-#endif
-
-void setup()
-{
-  //pinMode(servoPin, OUTPUT);
-  servo.attach(servoPin, 500, 1500);
-#ifdef SERVO_TO_LED
-  //pinMode(ledPin, OUTPUT);
-  servo2.attach(ledPin, 500, 1500);
-#elif defined TIMING_TO_LED
+void setup(void) {
   pinMode(ledPin, OUTPUT);
-#endif
-  int nothing = 0;
 }
 
-#ifdef SIMPLE_TEST
-void loop() {
-  servo.writeMicroseconds(1500);
-  delay(1000);
-  servo.writeMicroseconds(1000);
-  delay(1000);
-}
-
-#else
-void loop()
-{
-  servo.write(0);
-#ifdef SERVO_TO_LED
-  servo2.write(0);
-  delay(2000);
-#elif defined TIMING_TO_LED
-  ledValue = HIGH;
-  for (int i=0; i<2000/ledPulseDuration; i++) {
-    digitalWrite(ledPin, ledValue);
-    ledValue = !ledValue;
-    delay(ledPulseDuration);
+void loop(void) {
+  if (!initialized) {
+    servo.attach(servoPin);
+    servo.write(10);
+    initialized = true;
   }
-#endif
-
-  servo.write(180);
-#ifdef SERVO_TO_LED
-  servo2.write(180);
-  delay(2000);
-#elif defined TIMING_TO_LED
-  ledValue = HIGH;
-  for (int i=0; i<2000/ledPulseDuration; i++) {
-    digitalWrite(ledPin, ledValue);
-    ledValue = !ledValue;
-    delay(ledPulseDuration);
-  }
-#endif
-
-#ifdef TIMING_TO_LED
-  digitalWrite(ledPin, HIGH);
-#endif
-  for (pos=0; pos<180; pos++) {
+  for (int pos=0; pos<180; ++pos) {
+    if (++counter == 10) {
+      ledValue = !ledValue;
+      digitalWrite(ledPin, ledValue);
+      counter = 0;
+    }
     servo.write(pos);
-#ifdef SERVO_TO_LED
-    servo2.write(pos);
-#endif
-    delay(15);
-  }
-
-#ifdef TIMING_TO_LED
-  digitalWrite(ledPin, LOW);
-#endif
-  for (pos=180; pos>=1; pos--) {
-    servo.write(pos);
-#ifdef SERVO_TO_LED
-    servo2.write(pos);
-#endif
     delay(15);
   }
 }
-#endif
